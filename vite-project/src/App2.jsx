6c5ae599aa5dc98c11bdc967471314f5702ec405
Arnab199993 <103571005+Arnab199993@users.noreply.github.com>
2022-12-10 21:36:32 +0530

    final push

--- vite-project/src/App2.jsx
+++ vite-project/src/App2.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import axios from 'axios'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 const App2 = (props) => {
+  const [posts,setposts]=useState([])
     const [names,setnames]=useState("")
     const [array,setarray]=useState([]) 
-    axios.get("https://jsonplaceholder.typicode.com/users").then((response)=>{
+    useState(()=>{
+      axios.get("https://jsonplaceholder.typicode.com/users").then((response)=>{
         console.log(response.data)
+        setposts(response.data)
     }).catch((err)=>{
         console.log(err)
     }) 
+    },[])
     const  inputchange=(event)=>{
       setnames(event.target.value)
     //   console.log(event.target.value)
@@ -25,6 +29,11 @@ const App2 = (props) => {
       <label>Name</label>
        <input type={"text"} onChange={inputchange} ></input>
        <button onClick={add}>Add</button>
+       {
+        posts.map((post)=>(
+          <li key={post.id}>{post.name}</li>
+        ))
+       }
        {
         array.map((ele,i)=>(
          <li key={i}>{ele}</li>
